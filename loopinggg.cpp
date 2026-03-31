ab9e4c938245ed2787d9ec355b659d592a605d94
aryaom74-max <aryaom74@gmail.com>
2026-03-31 21:29:22 +0700

    Fix minor typo in language prompt

--- loopinggg.cpp
+++ loopinggg.cpp
@@ -7,7 +7,7 @@ int main() {
 
     cout << "Masukkan nilai Matematika = ";
     cin >> nilM;
-    cout << "Masukkan nilai Bahasa Inggria = ";
+    cout << "Masukkan nilai Bahasa Inggris = ";
     cin >> nilB;
 
     rerata = (nilB + nilM) / 2;
